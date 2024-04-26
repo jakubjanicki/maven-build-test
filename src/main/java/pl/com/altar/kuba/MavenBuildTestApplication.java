00094f11321a886f79f1a59812d666657a34edd2
Jakub Janicki <jakub.janicki@altar.com.pl>
2024-04-26 15:48:19 +0200

    firs release

---
+++ src/main/java/pl/com/altar/kuba/MavenBuildTestApplication.java
@@ -0,0 +1,13 @@
+package pl.com.altar.kuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenBuildTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenBuildTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/altar/kuba/MavenBuildTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.altar.kuba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenBuildTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
